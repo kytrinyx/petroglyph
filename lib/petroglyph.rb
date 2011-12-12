df546a934a4649eda1ead2d859ce726d4220faca
Katrina Owen <katrina.owen@gmail.com>
2011-12-12 13:21:29 +0100

    Add basic functionality for node

--- lib/petroglyph.rb
+++ lib/petroglyph.rb
@@ -2,7 +2,52 @@ require 'json'
 
 module Petroglyph
 
-  class Template
+  class Fragment
+    def initialize(parent_context, locals)
+      @parent_context = parent_context
+      @locals = locals
+      @root = {}
+    end
+
+    def node(name, value = nil, &block)
+      if block_given?
+        @root = @root.merge(name => yield)
+      else
+        @root = @root.merge(name => value)
+      end
+      @root
+    end
+
+    def merge(hash)
+      @root = @root.merge hash
+    end
+
+    def method_missing(method, *args, &block)
+      if @locals.has_key?(method)
+        @locals[method]
+      else
+        @parent_context.send method, *args, &block
+      end
+    end
+
   end
 
+  class Template
+
+    def self.build(locals = {}, &block)
+      parent_context = eval "self", block.binding
+      t = self.new
+      t.data = Fragment.new(parent_context, locals).instance_eval(&block)
+      t
+    end
+
+    def data=(data)
+      @data = data
+    end
+
+    def render
+      @data.to_json
+    end
+
+  end
 end
--- spec/petroglyph_spec.rb
+++ spec/petroglyph_spec.rb
@@ -3,23 +3,15 @@ require 'petroglyph'
 
 describe Petroglyph do
 
-  xit "adds a node with a simple value" do
+  it "adds a node with a simple value" do
     t = Petroglyph::Template.build do
-      node :drink, "hot"
+      node :drink, "tea"
     end
 
     t.render.should eq({:drink => "tea"}.to_json)
   end
 
-  xit "can use self to add a node with a simple value" do
-    t = Petroglyph::Template.build do
-      self[:drink] = "hot"
-    end
-
-    t.render.should eq({:drink => "tea"}.to_json)
-  end
-
-  xit "nests stuff arbitrarily deeply" do
+  it "nests stuff arbitrarily deeply" do
     t = Petroglyph::Template.build do
       node :drink do
         node :tea do
@@ -33,7 +25,7 @@ describe Petroglyph do
     t.render.should eq({:drink => {:tea => {:temperature => "hot"}}}.to_json)
   end
 
-  xit "uses regular ruby" do
+  it "uses regular ruby" do
     t = Petroglyph::Template.build do
 
       node :drink do
@@ -49,7 +41,7 @@ describe Petroglyph do
     t.render.should eq({:drink => {:tea => "HOT"}}.to_json)
   end
 
-  xit "merges in a hash" do
+  it "merges in a hash" do
     t = Petroglyph::Template.build do
       tea = {:tea => {:temperature => 'hot', :type => 'wulong'}}
       merge tea
@@ -58,7 +50,7 @@ describe Petroglyph do
     t.render.should eq({:tea => {:temperature => "hot", :type => 'wulong'}}.to_json)
   end
 
-  xit "takes local variables" do
+  it "takes local variables" do
     t = Petroglyph::Template.build(:temperature => 'hot') do
       node :tea do
         node(:temperature) { temperature }
@@ -68,7 +60,7 @@ describe Petroglyph do
     t.render.should eq({:tea => {:temperature => "hot"}}.to_json)
   end
 
-  xit "can handle helper methods" do
+  it "can handle helper methods" do
     def temperature
       "hot"
     end
@@ -82,7 +74,7 @@ describe Petroglyph do
     t.render.should eq({:tea => {:temperature => "hot"}}.to_json)
   end
 
-  xit "lets local variables take precedence over helper methods" do
+  it "lets local variables take precedence over helper methods" do
     def temperature
       "warm"
     end
@@ -96,7 +88,7 @@ describe Petroglyph do
     t.render.should eq({:tea => {:temperature => "hot"}}.to_json)
   end
 
-  xit "makes sibling nodes" do
+  it "makes sibling nodes" do
     t = Petroglyph::Template.build do
       node :drink do
         "tea"
@@ -149,4 +141,13 @@ describe Petroglyph do
 
     t.render.should eq({:drinks => [{:drink => {:type => 'tea', :temperature => 'hot'}, :preparation => 'Boil water, then wait.'}]}.to_json)
   end
+
+  xit "can use self to add a node with a simple value" do
+    t = Petroglyph::Template.build do
+      self[:drink] = "tea"
+    end
+
+    t.render.should eq({:drink => "tea"}.to_json)
+  end
+
 end
