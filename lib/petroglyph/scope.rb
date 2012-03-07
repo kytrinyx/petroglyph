b8fb805382fd4296a2d64a279d28c8df8b41fb9d
Katrina Owen <katrina.owen@gmail.com>
2012-03-07 19:06:43 +0100

    Add fix for nested partials. Fixes #10

--- lib/petroglyph/scope.rb
+++ lib/petroglyph/scope.rb
@@ -76,7 +76,7 @@ module Petroglyph
 
     def partial(name, locals = {})
       data = Petroglyph.partial(name, file)
-      scope = Scope.new(@context, locals)
+      scope = Scope.new(@context, locals, file)
       scope.instance_eval(data.to_s)
       merge scope.value
     end
