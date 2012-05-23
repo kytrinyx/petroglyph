71ddd648d47ff13fd1ee3af709b12f8e3cc48436
Katrina Owen <katrina.owen@gmail.com>
2012-05-23 14:39:53 +0200

    Set default content-type. Fixes #12

--- lib/sinatra/petroglyph.rb
+++ lib/sinatra/petroglyph.rb
@@ -2,6 +2,7 @@ require 'tilt/petroglyph'
 
 module Sinatra::Templates
   def pg(template, options={}, locals={})
+    options.merge!(:default_content_type => :json)
     render :pg, template, options, locals
   end
 end
